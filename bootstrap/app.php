6e3d839ac8129183d9c487c065945416a7790643
ExoticOnix <mshapirovskyy@gmail.com>
2025-04-30 18:17:56 +0300

    HEADER_X_FORWARDED_ALL

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,13 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
             AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        $middleware->trustProxies(at: '*', headers:
-            Request::HEADER_X_FORWARDED_FOR |
-            Request::HEADER_X_FORWARDED_HOST |
-            Request::HEADER_X_FORWARDED_PORT |
-            Request::HEADER_X_FORWARDED_PROTO |
-            Request::HEADER_X_FORWARDED_AWS_ELB
-        );
+        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_ALL);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
