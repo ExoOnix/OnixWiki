2ce6709d17391c62f695c9755decaf47c55a55c9
ExoticOnix <mshapirovskyy@gmail.com>
2025-04-30 18:41:23 +0300

    env fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if($this->app->environment('FORCE_HTTPS')) {
+        if(env('FORCE_HTTPS', false)) {
             \URL::forceScheme('https');
         }
     }
